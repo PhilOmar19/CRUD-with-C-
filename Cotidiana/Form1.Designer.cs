74ba25505532fedc119d53f12ef4a2c66ae8058a
PhilOmar19 <141745067+PhilOmar19@users.noreply.github.com>
2024-06-22 07:05:52 +0800

    Primer commit

---
+++ Cotidiana/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Cotidiana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkRed;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(11, 7, 7, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1617, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COTIDIANA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 460);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Añadir tarea";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(1013, 460);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Eliminar tarea";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.NavajoWhite;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tareas pendientes";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.NavajoWhite;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 541);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(261, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tareas completadas";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(19, 935);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(203, 52);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Eliminar tarea";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 85);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1028, 277);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 563);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1028, 276);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 384);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tarea";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(135, 385);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(549, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 423);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 23);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Descripción";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(135, 423);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(549, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Coral;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(220, 460);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(203, 52);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Cargar";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Coral;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(227, 935);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(203, 52);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Cargar";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(767, 425);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 23);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "ID";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(803, 425);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(61, 22);
+            this.textBox7.TabIndex = 20;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(13, 857);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 23);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "ID";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(51, 857);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(61, 22);
+            this.textBox8.TabIndex = 22;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.ForestGreen;
+            this.button7.Enabled = false;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(772, 460);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(236, 52);
+            this.button7.TabIndex = 23;
+            this.button7.Text = "Completar tarea";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1632, 1040);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Cotidiana/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cotidiana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("Server= DESKTOP-TFNBFUK; database= Cotidiana; integrated security= true");
+            conexion.Open();
+            string Tarea= textBox3.Text;
+            string Descripcion= textBox4.Text;
+            string cadena = "insert into Pendiente (Tarea, descripción) values ('"+Tarea+"', '"+Descripcion+"')";
+            SqlCommand comando = new SqlCommand(cadena,conexion);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Los datos se han guardado correctamente");
+            textBox3.Text = "";
+            textBox4.Text = "";
+            conexion.Close();
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox7.Text) && int.TryParse(textBox7.Text, out int result))
+            {
+                button2.Enabled = true;  
+                button7.Enabled = true;
+            }
+            else 
+            {
+                button2.Enabled = false;
+                button7.Enabled = false; 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("Server= DESKTOP-TFNBFUK; database= Cotidiana; integrated security= true");
+            conexion.Open();
+            string ID = textBox7.Text;
+            string cadena = "delete from Pendiente where ID="+ID;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            int cant;
+            cant= comando.ExecuteNonQuery();
+            if (cant == 1)
+            {
+                textBox7.Text = "";
+                MessageBox.Show("Tarea eliminada");
+            }
+            else
+                MessageBox.Show("Tarea no encontrada");
+            
+            conexion.Close();
+            button2.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            SqlConnection conexion = new SqlConnection("Server= DESKTOP-TFNBFUK; database= Cotidiana; integrated security= true");
+            conexion.Open();
+            string cadena = "Select ID, Tarea, Descripción from Pendiente";
+            SqlCommand comando= new SqlCommand(cadena, conexion);
+            SqlDataReader registros= comando.ExecuteReader();
+            while( registros.Read()) 
+            {
+                textBox1.AppendText(registros["ID"].ToString());
+                textBox1.AppendText("  -  ");
+                textBox1.AppendText(registros["Tarea"].ToString());
+                textBox1.AppendText("  -  ");
+                textBox1.AppendText(registros["Descripción"].ToString());
+                textBox1.AppendText(Environment.NewLine);
+            }
+            conexion.Close();
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            SqlConnection conexion = new SqlConnection("Server= DESKTOP-TFNBFUK; database= Cotidiana; integrated security= true");
+            conexion.Open();
+            string cadena = "Select ID, Tarea, Descripción from Completada";
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            SqlDataReader registros = comando.ExecuteReader();
+            while (registros.Read())
+            {
+                textBox2.AppendText(registros["ID"].ToString());
+                textBox2.AppendText("  -  ");
+                textBox2.AppendText(registros["Tarea"].ToString());
+                textBox2.AppendText("  -  ");
+                textBox2.AppendText(registros["Descripción"].ToString());
+                textBox2.AppendText(Environment.NewLine);
+            }
+            conexion.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("Server= DESKTOP-TFNBFUK; database= Cotidiana; integrated security= true");
+            conexion.Open();
+            string ID = textBox7.Text;
+            string cadena1 = "insert into Completada (Tarea, descripción) select Tarea, descripción from Pendiente where ID=" + ID;
+            string cadena2 = "delete from Pendiente WHERE ID=" + ID;
+            SqlCommand comando1 = new SqlCommand(cadena1, conexion);
+            SqlCommand comando2 = new SqlCommand(cadena2, conexion);
+
+            int cant1 = comando1.ExecuteNonQuery();
+            int cant2 = comando2.ExecuteNonQuery();
+
+            if (cant1 == 1 && cant2 == 1)
+            {
+                textBox7.Text = "";
+                MessageBox.Show("Tarea completada");
+            }
+            else
+            {
+                MessageBox.Show("Tarea no encontrada");
+            }
+
+            conexion.Close();
+            button7.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("Server= DESKTOP-TFNBFUK; database= Cotidiana; integrated security= true");
+            conexion.Open();
+            string ID = textBox8.Text;
+            string cadena = "delete from Completada where ID=" + ID;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            int cant;
+            cant = comando.ExecuteNonQuery();
+            if (cant == 1)
+            {
+                textBox7.Text = "";
+                MessageBox.Show("Tarea eliminada");
+            }
+            else
+                MessageBox.Show("Tarea no encontrada");
+
+            conexion.Close();
+            button4.Enabled = false;
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox8.Text) && int.TryParse(textBox8.Text, out int result))
+            {
+                button4.Enabled = true;
+            }
+            else
+            {
+                button4.Enabled = false;
+            }
+        }
+
+      
+    }
+}
